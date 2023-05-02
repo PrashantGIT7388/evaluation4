666e00bd73e1595cb0a3c5b987bafc06a103a948
PrashantGIT7388 <yd.pt180@gmail.com>
2023-05-02 16:02:53 +0530

    backend

---
+++ models/post.Model.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const PostSchema = mongoose.Schema({
+  title: String,
+  body: String,
+  device: String,
+});
+
+const PostModel = mongoose.model("post", PostSchema);
+
+module.exports = { PostModel };
